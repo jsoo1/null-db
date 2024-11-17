b99bd57f903690e6caaac1d52c84e7d1ed43f100
John Soo <jsoo1@asu.edu>
2024-11-17 16:29:55 -0700

    database: Add basic-raft nixos-test

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,3 +1,8 @@
 final: prev: {
-  null-db = final.callPackage ../database { };
+  null-db = (final.callPackage ../database { }).overrideAttrs (o: {
+    # passthru is a nixpkgs field that is reserved to only exist in
+    # nix runtime but not end up in a derivation. passthru.tests is
+    # one very common attribute.
+    passthru.tests.basic-raft = final.callPackage ./nixos-tests/basic-raft { };
+  });
 }
